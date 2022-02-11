66d9406b56d9f253f4377959418142aa67120929
AtanasVA <99086114+AtanasVA@users.noreply.github.com>
2022-02-12 01:00:08 +0200

    Add files via upload

---
+++ RemoveElement/script.js
@@ -0,0 +1,19 @@
+/*
+Given an integer array nums and an integer val, 
+remove all occurrences of val in nums.
+*/
+
+const nums = [0,1,2,2,3,0,4,2];
+const val = 2;
+var removeElement = function(nums, val) {
+    let editedNums = [];
+
+    for (let i = 0; i < nums.length; i++){
+        if(nums[i] == val) continue;
+        editedNums.push(nums[i]);
+    };
+    const k = editedNums.length;
+    return k, editedNums;
+};
+
+console.log(removeElement(nums,val));
