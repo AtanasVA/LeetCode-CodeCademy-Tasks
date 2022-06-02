15b8663d5425b2a2288b6ce1cb70faaa15ea80c0
Atanas Vasilev <99086114+AtanasVA@users.noreply.github.com>
2022-06-02 14:07:39 +0300

    Update script.js

--- Palindromes/script.js
+++ Palindromes/script.js
@@ -11,9 +11,3 @@ const isPalindrome = function(x) {
 };
 
 console.log(isPalindrome(121));
-
-/*
-1) Split number to string and store in array
-2) Reverse the array and store in a new variable
-3) Compare each digit seperately
-*/
