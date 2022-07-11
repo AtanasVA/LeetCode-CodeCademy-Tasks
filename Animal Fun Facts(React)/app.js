112a049d3fd585e7fc1175144034ec2fc5fd9498
Atanas Vasilev <99086114+AtanasVA@users.noreply.github.com>
2022-07-11 10:30:05 +0300

    Create app.js

---
+++ Animal Fun Facts(React)/app.js
@@ -0,0 +1,51 @@
+import { animals } from './animals';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+const title = '';
+const showBackground = false;
+const background = (
+  <img
+    className='background' 
+    alt='ocean' 
+    src='/images/ocean.jpg'
+  />
+);
+const images =[]
+const displayFact = (e) =>{
+  const animalClicked = e.target.alt;
+  const animalInfo = animals[animalClicked]
+  const randIndex = Math.floor(Math.random() * animalInfo.facts.length);
+
+  const funFact = animalInfo.facts[randIndex];
+  document.getElementById('fact').innerHTML = funFact;
+};
+for(const animal in animals){
+  images.push((
+    <img 
+      key ={animal} 
+      className ='animal' 
+      alt={animal} 
+      src ={animals[animal].image} 
+      aria-label={animal} 
+      role='button' 
+      onClick={displayFact}
+    />
+  ))
+}
+const animalFacts = (
+<div>
+  <h1>
+  {title==='' ? 'Click an animal for a fun fact' : title}
+  </h1>
+  {showBackground && background}
+  <p id='fact'>
+  </p>
+  <div className='animals'>
+    {images}
+  </div>
+ </div> 
+);
+
+ReactDOM.render(animalFacts, document.getElementById('root'));
